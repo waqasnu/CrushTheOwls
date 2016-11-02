4f02f53acc7705d2360636792c12f911b978a7ad
waqasnu <waqasnu>
2016-11-02 23:37:03 +0500

    waq

---
+++ Assets/_Yurowm/Match-Tree Engine/Scripts/playat.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.Advertisements;
+using System.Collections;
+
+public class playat : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+		InvokeRepeating("LaunchProjectile", 10,8);
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void LaunchProjectile () {
+
+		Advertisement.Show ();
+
+	}
+}
